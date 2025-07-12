39840e43b799ea67f4bba8de398f1cafd764735b
Chinh Le <chinhle.120393@gmail.com>
2025-07-12 17:40:07 +0700

    fix: navigation for gh pages

--- app/src/pages/_app.tsx
+++ app/src/pages/_app.tsx
@@ -3,10 +3,17 @@ import '../generated/globals.css';
 
 import type { AppProps } from 'next/app';
 import HotReload from '@wse/global/components/hot-reload';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1, viewport-fit=cover"
+        />
+      </Head>
       <Component {...pageProps} />
       <HotReload />
     </>
--- app/src/pages/_document.tsx
+++ app/src/pages/_document.tsx
@@ -4,10 +4,6 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, maximum-scale=1, viewport-fit=cover"
-        />
         <link rel="icon" href="/favicon.ico" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         {/* Fonts preloading */}
