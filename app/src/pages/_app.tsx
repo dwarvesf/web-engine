3c38fc2a65b210833bbae305d85c5c4cc8b0f3cc
Chinh Le <71743905+chinhld12@users.noreply.github.com>
2025-07-18 17:51:16 +0700

    feat: csr (#12)

--- app/src/pages/_app.tsx
+++ app/src/pages/_app.tsx
@@ -4,12 +4,18 @@ import '../generated/globals.css';
 import type { AppProps } from 'next/app';
 import HotReload from '@wse/global/components/hot-reload';
 import SEO from '@wse/global/components/SEO';
+import AutoScrollToTop from '@wse/global/components/auto-scroll-top';
+import GlobalRouteHandler from '@wse/global/components/global-route-handler';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <SEO frontmatter={pageProps.frontmatter} />
-      <Component {...pageProps} />
+      <GlobalRouteHandler>
+        <AutoScrollToTop>
+          <Component {...pageProps} />
+        </AutoScrollToTop>
+      </GlobalRouteHandler>
       <HotReload />
     </>
   );
