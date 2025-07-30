f901c8b61948e81905091be6956ec4a248d82618
Chinh Le <chinhle.120393@gmail.com>
2025-07-30 10:11:14 +0700

    fix: wrapping primitive elements

--- app/src/global/utils/mdx-processing/mdx-remarks/remark-unwrap-custom-blocks.ts
+++ app/src/global/utils/mdx-processing/mdx-remarks/remark-unwrap-custom-blocks.ts
@@ -4,14 +4,45 @@ import type { Parent } from 'unist';
 import type { MdxJsxFlowElement } from 'mdast-util-mdx-jsx';
 import type { Paragraph, BlockContent, DefinitionContent } from 'mdast';
 
+const primitiveElements = [
+  // Navigation elements
+  'p',
+  'a',
+  'button',
+  'paragraph',
+  'anchor',
+  'link',
+  // Other typo elements
+  'h1',
+  'h2',
+  'h3',
+  'h4',
+  'h5',
+  'h6',
+  'ul',
+  'ol',
+  // table elements
+  'table',
+  'thead',
+  'tbody',
+  'tr',
+  // other elements
+  'hr',
+  'img',
+  'svg',
+  'video',
+  'audio',
+  'canvas',
+];
+
 export function remarkUnwrapCustomBlocks() {
   return (tree: Node) => {
     visit(
       tree,
       'mdxJsxFlowElement',
-      (node: MdxJsxFlowElement, index, parent: Parent) => {
+      (node: MdxJsxFlowElement, _index, parent: Parent) => {
         const typesChecking = [node.name, parent.type];
-        const whiteList = ['paragraph', 'anchor', 'button', 'p', 'a', 'link']
+        const whiteList = primitiveElements
           .map(i => [i, i.slice(0, 1).toUpperCase() + i.slice(1)])
           .flat();
         // If the node is an MDX JSX flow element,
